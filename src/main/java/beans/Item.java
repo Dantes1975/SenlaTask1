eefc1ff89185249cf18975355cfd2c161b047d57
Dantes1975 <dantes1975@inbox.ru>
2020-03-10 09:40:51 +0300

    Backpack-Upgrade-10032020

--- src/main/java/beans/Item.java
+++ src/main/java/beans/Item.java
@@ -4,13 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Item {
     private String name;
     private int weight;
     private int cost;
 
+    public Item() {
+    }
 
+    public Item(String name, int weight, int cost) {
+        this.name = name;
+        this.weight = weight;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
